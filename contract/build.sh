77457cefd0bf4a03a4cd6ae6ca72c8d5567490ab
Marcelo Fornet <mfornet94@gmail.com>
2024-12-16 12:20:16 +0100

    contract: build command

--- contract/build.sh
+++ contract/build.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-cargo build --target wasm32-unknown-unknown --release
-rm -rf build
-mkdir -p build
-cp target/wasm32-unknown-unknown/release/payment_channel.wasm build/
+cargo near build --no-docker --no-abi
