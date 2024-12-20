904abdb4a273c9c2f34e1c9404750c8f570ef53b
Alexander Comerford <alex@taoa.io>
2024-12-20 15:27:06 +0000

    Add constants

--- provider/src/lib.rs
+++ provider/src/lib.rs
@@ -4,6 +4,8 @@ pub mod db;
 pub mod errors;
 pub mod service;
 
+use std::time::Duration;
+
 pub use crate::background::*;
 pub use crate::common::*;
 pub use crate::db::*;
@@ -22,3 +24,12 @@ pub const PAYMENTS_HEADER_NAME: &str = "X-Payments-Signature";
 // When a channel is closed, the receiver / sender account id is set to this value
 pub const CLOSED_CHANNEL_ACCOUNT_ID: &str =
     "0000000000000000000000000000000000000000000000000000000000000000";
+
+// Amount of time until a channel is considered stale and the state should be
+// refreshed from the contract
+pub const STALE_CHANNEL_THRESHOLD: Duration = Duration::from_secs(30); // 30 seconds
+
+// Copied from the contract code
+pub const SECOND: u64 = 1_000_000_000;
+pub const DAY: u64 = 24 * 60 * 60 * SECOND;
+pub const HARD_CLOSE_TIMEOUT: u64 = 7 * DAY;
