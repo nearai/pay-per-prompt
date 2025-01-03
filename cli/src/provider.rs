b0a0f4aef13ab2432c3cf2946cd90d7934d20f1c
Alexander Comerford <alex@taoa.io>
2025-01-03 00:16:59 +0000

    more provider methods

--- cli/src/provider.rs
+++ cli/src/provider.rs
@@ -1,5 +1,5 @@
 use near_crypto::PublicKey;
-use near_sdk::AccountId;
+use near_sdk::{json_types::U128, AccountId};
 use serde::{Deserialize, Serialize};
 
 use crate::config::SignedState;
@@ -12,6 +12,11 @@ pub struct Provider {
 pub struct Details {
     pub account_id: AccountId,
     pub public_key: PublicKey,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
+pub struct SpentBalance {
+    pub spent_balance: U128,
 }
 
 impl Provider {
@@ -28,12 +33,30 @@ impl Provider {
             .unwrap()
     }
 
-    pub async fn close_payload(&self, channel_id: &str) -> SignedState {
-        reqwest::get(format!("{}/pc/close/{}", self.provider_url, channel_id))
+    pub async fn spent_balance(&self, channel_id: &str) -> SpentBalance {
+        reqwest::get(format!("{}/pc/state/{}", self.provider_url, channel_id))
             .await
             .unwrap()
-            .json::<SignedState>()
+            .json::<SpentBalance>()
             .await
             .unwrap()
+    }
+
+    pub async fn close_payload(&self, channel_id: &str, signed_state_payload: &str) -> SignedState {
+        let client = reqwest::Client::new();
+        let response = client
+            .post(format!("{}/pc/close/{}", self.provider_url, channel_id))
+            .body(signed_state_payload.to_string())
+            .send()
+            .await
+            .unwrap();
+        if response.status().is_success() {
+            return response.json::<SignedState>().await.unwrap();
+        } else {
+            panic!(
+                "Failed to close channel: {}",
+                response.text().await.unwrap()
+            );
+        }
     }
 }
